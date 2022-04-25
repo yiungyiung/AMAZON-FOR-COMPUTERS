0c6209836aaa02ad410effb71f3123a9a627e8cf
yiungyiung <yashparkhi.super@gmail.com>
2022-04-26 00:18:18 +0530

    masterrr

---
+++ src/pages/jsondata/Data2.js
@@ -0,0 +1,74 @@
+const Data = {
+    product: [{
+            "id": 1,
+            "name": "yash",
+            "type": "CPU",
+            "price": 100
+        },
+        {
+            "id": 2,
+            "name": "yash2",
+            "type": "GPU",
+            "price": 100
+        },
+        {
+            "id": 3,
+            "name": "yash3",
+            "type": "CPU",
+            "price": 100
+        },
+        {
+            "id": 4,
+            "name": "yash4",
+            "type": "GPU",
+            "price": 100
+        },
+        {
+            "id": 5,
+            "name": "yashy",
+            "type": "MOBO",
+            "price": 12000
+        },
+        {
+            "name": "yash6"
+        },
+        {
+            "name": "yash7"
+        },
+        {
+            "name": "yash8"
+        },
+        {
+            "name": "tanay"
+        },
+        {
+            "name": "hsay"
+        },
+        {
+            "name": "hsay2"
+        },
+        {
+            "name": "hsay3"
+        },
+        {
+            "name": "hsay4"
+        },
+        {
+            "name": "hsay5"
+        },
+        {
+            "name": "hsay6"
+        },
+        {
+            "name": "hsay7"
+        },
+        {
+            "name": "hsay8"
+        },
+        {
+            "name": "tanay"
+        }
+    ],
+};
+
+export default Data;
